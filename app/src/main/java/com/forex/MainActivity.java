73a366afe81744d38c80f560475fb023180354fc
Nikshail Infotech <3dshailen@gmail.com>
2023-11-01 20:29:46 +0530

    1.0

--- app/src/main/java/com/forex/MainActivity.java
+++ app/src/main/java/com/forex/MainActivity.java
@@ -13,6 +13,7 @@ import com.forex.Fragment.TradeFragment;
 import com.forex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    //from home 01 11 2023
     ActivityMainBinding binding;
     SessionManager sessionManager;
     @Override
